b17270738d67c032056cd2d80f8a83c678288592
Akshay <39487594+akshay-devadiga@users.noreply.github.com>
2022-02-11 13:35:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Box, Text, Heading } from "@chakra-ui/react";
+
+export default function App() {
+  return (
+    <Box minH="100vh" p={5}>
+      <Heading color="teal.400">Hello CodeSandbox</Heading>
+      <Text>Start editing to see some magic happen!</Text>
+    </Box>
+  );
+}
